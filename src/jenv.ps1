8af3d3ade07d17442e32870090a6134d0dca0e3d
Rodrigo Carvalho Silva <rodrigo.carvalho@bcb.gov.br>
2022-09-19 15:22:34 -0300

    Added option to accept defaults to "autoscan"

--- src/jenv.ps1
+++ src/jenv.ps1
@@ -28,6 +28,10 @@ param (
     [Alias("o")]
     [Switch]$output,
 
+    # Accept defaults
+    [Alias("y")]
+    [Switch]$yes,
+
     [parameter(mandatory = $false, position = 1, ValueFromRemainingArguments = $true)]$arguments
 )
 
@@ -121,15 +125,15 @@ if ($output) {
 #endregion
 
 if ($help -and $action -eq "") {
-    Write-Host '"jenv list"                 List all registered Java-Envs.'
-    Write-Host '"jenv add <name> <path>"    Adds a new Java-Version to JEnv which can be refferenced by the given name'
-    Write-Host '"jenv remove <name>"        Removes the specified Java-Version from JEnv'
-    Write-Host '"jenv change <name>"        Applys the given Java-Version globaly for all restarted shells and this one'
-    Write-Host '"jenv use <name>"           Applys the given Java-Version locally for the current shell'
-    Write-Host '"jenv local <name>"         Will use the given Java-Version whenever in this folder. Will set the Java-version for all subfolders as well'
-    Write-Host '"jenv link <executable>"    Creates shortcuts for executables inside JAVA_HOME. For example "javac"'
-    Write-Host '"jenv uninstall <name>"     Deletes JEnv and restores the specified java version to the system. You may keep your config file'
-    Write-Host '"jenv autoscan ?<path>?"    Will scan the given path for java installations and ask to add them to JEnv. Path is optional'
+    Write-Host '"jenv list"                            List all registered Java-Envs.'
+    Write-Host '"jenv add <name> <path>"               Adds a new Java-Version to JEnv which can be refferenced by the given name'
+    Write-Host '"jenv remove <name>"                   Removes the specified Java-Version from JEnv'
+    Write-Host '"jenv change <name>"                   Applys the given Java-Version globaly for all restarted shells and this one'
+    Write-Host '"jenv use <name>"                      Applys the given Java-Version locally for the current shell'
+    Write-Host '"jenv local <name>"                    Will use the given Java-Version whenever in this folder. Will set the Java-version for all subfolders as well'
+    Write-Host '"jenv link <executable>"               Creates shortcuts for executables inside JAVA_HOME. For example "javac"'
+    Write-Host '"jenv uninstall <name>"                Deletes JEnv and restores the specified java version to the system. You may keep your config file'
+    Write-Host '"jenv autoscan [--yes|-y] ?<path>?"    Will scan the given path for java installations and ask to add them to JEnv. Path is optional and "--yes|-y" accepts defaults.'
     Write-Host 'Get help for individual commands using "jenv <list/add/remove/change/use/local> --help"'
 }
 else {
@@ -146,7 +150,7 @@ else {
         getjava { Get-Java $config }
         link { Invoke-Link $help @arguments }
         uninstall { Invoke-Uninstall $config $help @arguments }
-        autoscan { Invoke-AutoScan $config $help @arguments }
+        autoscan { Invoke-AutoScan $config $help $yes @arguments }
     }
 
     #region Save the config
